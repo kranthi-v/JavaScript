d94e38eab672d378b762893901938e31a800701f
kranthi-v <krkranthikumar2003@gmail.com>
2025-10-15 10:43:53 +0530

    fact

--- factorial.js
+++ factorial.js
@@ -1,23 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script>
-        function main()
-        {
-            let n = 10
-            for(let i=1; i<=n; i++)
-            {
-                if(n%i==0)
-                console.log(i)
-            }
-        }
-        main()
-    </script>
-</head>
-<body>
-    
-</body>
-</html>
+function main()
+{
+    let n = 10
+    for(let i=1; i<=n; i++)
+    {
+        if(n%i==0)
+        console.log(i)
+    }
+}
+main()
